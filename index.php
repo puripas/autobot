c1473b8bd57138f2c99d6180f48409df4189653d
puripas <32860069+puripas@users.noreply.github.com>
2017-10-31 13:22:49 +0700

    comment

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once('./vendor/autoload.php');
+
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
@@ -12,33 +14,24 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
-
     // Loop through each event
     foreach ($events['events'] as $event) {
-
         // Line API send a lot of event type, we interested in message only.
         if ($event['type'] == 'message') {
+               switch($event['message']['type']) {
+                 case 'text':
+                      // Get replyToken
+                      $replyToken = $event['replyToken'];
 
-            // Get replyToken
-            $replyToken = $event['replyToken'];
+                      // Reply message
+                      $respMessage = 'Hello, your message is '. $event['message']['text'];
 
-            switch($event['message']['type']) {
-
-                case 'image':
-                    $messageID = $event['message']['id'];
-                    $respMessage = 'Hello, your image ID is '. $messageID;
-
-                    break;
-                default:
-                    $respMessage = 'Please send image only';
-                    break;
+                      $httpClient = new CurlHTTPClient($channel_token);
+                      $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                      $textMessageBuilder = new TextMessageBuilder($respMessage);
+                      $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                 break;
             }
-
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
         }
     }
 }
